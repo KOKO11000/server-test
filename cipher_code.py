1272dfc9ebcfd95fe5975dab3dfd7de09c6a68fc
Israel_Winnett <arulik98@gmail.com>
2025-11-13 12:46:15 +0200

    I hate this sest i didn't know anithing

--- cipher_code.py
+++ cipher_code.py
@@ -1,15 +1,17 @@
 from string import ascii_lowercase, ascii_uppercase
 
-offset = 3
-class CaesarCode:    
-    def caesar_code(messege: str):
+
+class CaesarCode: 
+    def caesar_code(messege: str, offset: int):
         for i in messege:
             if i in ascii_uppercase:
-                (24 + offset) % len(ascii_uppercase) == 2
-                messege[i] = ascii_uppercase[i]+offset
+                (24 + offset) % len(ascii_uppercase) == 2                
+                messege.index(i) == ascii_uppercase.index(i) + offset
+            
             elif i in ascii_lowercase:
                 (24 + offset) % len(ascii_uppercase) == 2
-                messege[i] = ascii_lowercase[i]+offset
+                caesar_code = ascii_lowercase.index(i)+ offset
+                messege.index(i) == caesar_code
             return messege
 
 
@@ -24,7 +26,7 @@ class RailFenceCipher:
         return messege
     
     def removes_spaces(messege_with_spaces: get_a_messege):
-        messege_without_spaces = messege_with_spaces.strip(" ")
+        messege_without_spaces = messege_with_spaces.repalce(" ", "")
         return messege_without_spaces
     
     def separate_even_letters(messge_without_spaces: removes_spaces):
--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from fastapi import FastAPI
 import uvicorn
+from cipher_code import CaesarCode
 app = FastAPI()
-
+a = CaesarCode.caesar_code("hello world",3)
 @app.get("/test")
 def start(s):
     return {"messege": "welcome user!"}
 
 @app.get("/test/:{name}")
 def save_user(s):
+    with open("name.txt", "a") as f:
+        pass
     return {"messege": "saved user"}
 
 
-@app.post("/caasar")
+@app.post("/caesar")
 def caesar(s):
     pass
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)
+    print(a)
